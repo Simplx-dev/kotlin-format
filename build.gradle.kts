1ee72c96ed2552e39fe9eaafab27ac49fc60b8ee
Simone Giacomelli <simone.giacomelli@gmail.com>
2021-11-08 21:24:04 +0100

    removed unused conf lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,7 @@ repositories {
     mavenCentral()
 }
 
-sourceSets {
-    main {
-        java.srcDir("src/main")
-    }
-    test {
-        java.srcDir("src/test")
-    }
-}
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
-//    testImplementation(kotlin("test-testng"))
 }
