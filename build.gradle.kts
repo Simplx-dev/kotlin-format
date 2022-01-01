9917709f571ebd33dc3e3581b12194b465d8274c
Bohdan Bezpartochnyi <bbezpartochnyi@transcenda.com>
2022-01-01 22:08:32 +0200

    Prepare for jitpack publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,26 @@
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.10"
+    `maven-publish`
 }
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.simplx"
+            artifactId = "kotlin-format"
+            version = "1.0"
+        }
+    }
 }
